bd538fd19176db9bd8cca063c4d8f689332a0f66
dzz_cat <dzz_cat@mail.ru>
2023-05-26 10:04:57 +0400

    add 64/

---
+++ 64/Program.cs
@@ -0,0 +1,33 @@
+﻿// // Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+
+
+int GetNumberFromUser(string text)
+{
+    Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool NaturalNumber(int number)
+{
+    if(number > 0) return true;
+    else return false;
+}
+
+void ValuesFromNToOne(int number)
+{
+    
+    if(number > 0)
+    {
+        Console.Write(number + " ");
+        ValuesFromNToOne(number - 1);
+    } 
+}
+
+int n = GetNumberFromUser("Введите число > ");
+Console.WriteLine("Результат: ");
+if(NaturalNumber(n)) ValuesFromNToOne(n);
+else Console.WriteLine("Введено отрицательное число");
